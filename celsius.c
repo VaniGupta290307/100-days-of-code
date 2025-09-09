0b15e0455304fa53fcf8c2cc8a63b856d2c15085
VaniGupta290307 <vanigupta873@gmail.com>
2025-09-09 21:51:58 +0530

    day3 Q5
    Q5: Write a program to convert temperature from Celsius to Fahrenheit.

---
+++ celsius.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float C,F;
+    printf("enter temperature in celsius");
+    scanf("%f",&C);
+    F=(9/5*C) +32;
+    printf("Temperature in Fahrenite is %f",F);
+    return 0;
+
+}
