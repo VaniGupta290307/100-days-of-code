46ff434689b89ae51a12f44b4dfb2683565ddd00
VaniGupta290307 <vanigupta873@gmail.com>
2025-11-19 17:41:31 +0530

    Day37Q74.c
    Q74: Find the transpose of a matrix.

---
+++ Day37Q74.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[50][50], t[50][50];
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            t[j][i] = a[i][j];
+        }
+    }
+    for(int i = 0; i < c; i++) {
+        for(int j = 0; j < r; j++) {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
