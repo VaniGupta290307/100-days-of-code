41216756f4be320203d385ec53f6470ba115a030
VaniGupta290307 <vanigupta873@gmail.com>
2025-11-21 23:20:56 +0530

    Create Day51Q102.c

---
+++ Day51Q102.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int findCeilIndex(int arr[], int n, int x) {
+    int low = 0, high = n - 1;
+    int result = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] >= x) {
+            result = mid;       
+            high = mid - 1;     
+        } else {
+            low = mid + 1;      
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int n, x;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &x);
+
+    int ans = findCeilIndex(arr, n, x);
+    printf("%d", ans);
+
+    return 0;
+}
